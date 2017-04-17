3b1b5164a6e0272914a28a6bb70493a53c9e642b
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-04-17 14:01:13 -0600

    undo re-write and fix state

--- src/gsp/GSP.java
+++ src/gsp/GSP.java
@@ -21,11 +21,13 @@ public class GSP<T> {
     private State goal;
     private ArrayList<Action> plan;
     private ArrayList<Action> actions;
+    private Stack stack;
     public GSP(State init, State goal, ArrayList<Action> actions) {
         this.state = init;
         this.goal = goal;
         this.actions = actions;
         this.plan = new ArrayList();
+        this.stack = new Stack();
         this.state.findBlocks();
         System.out.println("");
         run();
@@ -33,7 +35,6 @@ public class GSP<T> {
     
     
     private void run() {
-        Stack<T> stack = new Stack();
         System.out.println(this.state);
         System.out.println(this.goal);
         System.out.println("");
@@ -81,7 +82,24 @@ public class GSP<T> {
                     }
 
                 }
-                //fix state
+                this.state.setState(copyState);
+                this.state.findBlocks();
+                //fix state, sometimes the state is not complete
+                for (String block: this.state.getBlocks()) {
+                    boolean clear = true; 
+                    for (Predicate curState: this.state.getState()) {
+                        if (curState.getIdentifier().equals("On")) {
+                            if (curState.getParams().get(1).equals(block)) {
+                                clear = false;
+                            }
+                        }
+                    }
+                    if (clear && !block.isEmpty()) {
+                        Predicate newP = new Predicate("Clear("+block+")");
+                        if (!copyState.contains(newP))
+                            copyState.add(newP);
+                    }
+                }
                 
                 this.state.setState(copyState);
                 plan.add((Action)actualValue);
@@ -105,117 +123,187 @@ public class GSP<T> {
     public ArrayList<Action> getRelevantAction(Predicate p) {
         ArrayList<Action> ac = new ArrayList();
         
-        ArrayList<Action> cloned = this.generateAvailableActions();
-        for (Action a: cloned) {
-            if (!this.plan.isEmpty()){
-                if (a.getIdentifier().equals(this.plan.get(this.plan.size()-1).getIdentifier())) {
-                    continue;
-                }
+        ArrayList<Action> cloned = new ArrayList();
+        for(Action a : this.actions)
+            cloned.add(a.cloneCustom());
+        
+        Action a = null;
+        if (p.getIdentifier().equals("On")) {
+            a = cloned.get(0);
+        }
+        if (p.getIdentifier().equals("OnTable") || p.getIdentifier().equals("AE")) {
+            a = cloned.get(3);
+        }
+        if (p.getIdentifier().equals("Hold")) {
+            //verificar si pickup cumple en el estado actual
+            a = cloned.get(2);
+            boolean valid = verifyState(a);
+            if (!valid) {
+                a = cloned.get(1);
+//                if (!plan.isEmpty()){
+//                    if (a.getIdentifier().equals(plan.get(plan.size()-1).getIdentifier())) {
+//                        a = cloned.get(3);
+//                        
+//                        boolean valid1 = modifyAction(a, p, a.getPrecond());
+//                        boolean valid2 = modifyAction(a, p, a.getEffects());
+//                        if (valid1 && valid2 && ac.size() < 1){
+//                            a.setParams(p.getParams());
+//                            ac.add(a);
+//                        }
+//                        return ac;
+//                    } 
+//                }
+//                
             }
-            ArrayList<Predicate> eff = a.getEffects();
-            for (Predicate effect: eff) {
-                if (effect.getIdentifier().equals(p.getIdentifier()) && effect.getCantParams() == p.getCantParams()){
-                    ac.add(a);
-                }
-
+            //si no es unstack
+        }
+        if (p.getIdentifier().equals("Clear")) {
+            //verificar si putdown cumple en el estado actual
+            //si no es stack
+            a = cloned.get(3);
+            boolean valid = verifyState(a);
+            if (!valid) {
+                a = cloned.get(1);
             }
             
         }
+        
+        ArrayList<Predicate> eff = a.getEffects();
+        for (Predicate effect: eff) {
+            if (effect.getIdentifier().equals(p.getIdentifier()) && effect.getCantParams() == p.getCantParams()){
+                String id = p.getIdentifier();
+                boolean valid = modifyAction(a, p, a.getPrecond());
+                boolean valid2 = modifyAction(a, p, a.getEffects());
+                if (valid && valid2 && ac.size() < 1){
+                    a.setParams(p.getParams());
+                    ac.add(a);
+                }
+            }
+
+        }
+            
+        
         return ac;
     }
     
-    
-    public boolean modifyAction(Action a, ArrayList<String> newParams, ArrayList<Predicate> predicates) {
-        ArrayList action_params = a.getParams();
-        
-        
-        if (action_params.size() != newParams.size()) {
-            return false;
-        }
-
-        for (Predicate precond: predicates) {
-            for (int i = 0; i < action_params.size(); i++) {
-                for (int j = 0; j < precond.getParams().size();j++) {
-                    if ( precond.getParams().get(j).equals(action_params.get(i)) && precond.valid(j)) {
-                        
-                        precond.getParams().set(j, newParams.get(i));
-                        precond.addEvalIndex(j);
-                    }
+    public boolean verifyState(Action a){
+        ArrayList<Predicate> eff = a.getPrecond();
+        boolean valid = true;
+        for (Predicate effect: eff) {
+            boolean innerValid = false;
+            if (effect.getIdentifier().contains("^")) {
+                continue;
+            }
+            for (Predicate currenState: state.getState()){
+                if (effect.equals(currenState)){
+                    innerValid = true;
                 }
             }
-            precond.clearEval();
+            
+            if (innerValid == false) {
+                valid = false;
+            }
+
         }
-        
-        return true;
+        return valid;
     }
     
     
-    public ArrayList<Action> generateAvailableActions() {
-        ArrayList<Action> returnArray = new ArrayList();
-        for (Action action: this.actions) {
-            
-            HashSet<Variable> availableVariables = new HashSet();
-            ArrayList<String> identifiersEvaluated = new ArrayList();
-            
-            ArrayList<Boolean> valids = new ArrayList();
-            for (Predicate preCondition: action.getPrecond()) {
-                if (preCondition.getIdentifier().contains("^")) {
-                    continue;
-                }
-                for (Predicate statePredicate: this.state.getState()) {
-                    if (statePredicate.getIdentifier().equals(preCondition.getIdentifier())) {
-                        if (!identifiersEvaluated.contains(statePredicate.getIdentifier())) {
-                            identifiersEvaluated.add(statePredicate.getIdentifier());
-                            valids.add(true);
-                            for (String pParams: action.getParams()) {
-                                for (int i = 0; i < statePredicate.getParams().size();i++) {
-                                    String aParams = statePredicate.getParams().get(i);
-                                    if (pParams.equals(aParams)) {
-                                        availableVariables.add(new Variable(aParams, i));
-                                    }
-
+    public boolean modifyAction(Action a, Predicate p, ArrayList<Predicate> predicates) {
+        ArrayList action_params = a.getParams();
+        
+        ArrayList<String> newParams = p.getParams();
+        //revisar los params del predicado relevante y la acción
+        if (newParams.size() != action_params.size()) {
+           //check preconditions of current action on current state
+            ArrayList<Predicate> predicatesSatisfy = new ArrayList();
+            ArrayList<String> rep = new ArrayList();
+            ArrayList<Predicate> repeated = new ArrayList();
+            for (Predicate precondition: a.getPrecond()) {
+                for (Predicate prState: state.getState()) {
+                    if (precondition.getIdentifier().equals(prState.getIdentifier())) {
+                        if (rep.contains(prState.getIdentifier())) {
+                            repeated.add(prState);
+                            int iR = -1; //repeated index
+                            for (int i = 0; i <  predicatesSatisfy.size();i++) {
+                                if ( predicatesSatisfy.get(i).getIdentifier().equals(prState.getIdentifier())) {
+                                    //remove and add to repeated
+                                    iR = i;
                                 }
-                            } 
+                            }
+                            if (iR != -1) {
+                                Predicate removed = predicatesSatisfy.remove(iR);
+                                repeated.add(removed);
+                            }
+                        } else {
+                            predicatesSatisfy.add(prState);
+                            rep.add(prState.getIdentifier());
                         }
-                        
-                    } 
+                    }
                 }
             }
-            if (action.getPrecond().size() - 1 == valids.size()) {
-                
-                Action newAction = new Action(action.getIdentifier());
-                ArrayList<Predicate> preconds = new ArrayList();
-                ArrayList<Predicate> effects = new ArrayList();
-                for (Predicate precond: action.getPrecond()) {
-                    preconds.add(precond.cloneCustom());
+            //if no enough preconditions were found in current state for current action
+            if (a.getPrecond().size() - 1 > predicatesSatisfy.size() + repeated.size()) {
+                return false;
+            }
+            //check the combinations of the predicates
+            //and keep the one that generates the desired predicate p
+            ArrayList<Predicate> valid = new ArrayList();
+            System.out.println(repeated);
+            System.out.println(predicatesSatisfy);
+            if (a.getIdentifier().equals("Unstack")) {
+                for (Predicate satisfy: predicatesSatisfy) {
+                    for (Predicate reped: repeated) {
+                        ArrayList sParams = satisfy.getParams();
+                        ArrayList rParams = reped.getParams();
+                        if ((reped.getIdentifier().equals("Clear") && satisfy.getIdentifier().equals("On") )||
+                            (reped.getIdentifier().equals("On") && satisfy.getIdentifier().equals("Clear") )) {
+                            if (sParams.get(0).equals(rParams.get(0))) {
+                                valid.add(reped);
+                            }
+                            
+                        }
+                    }
                 }
-                for (Predicate eff: action.getEffects()) {
-                    effects.add(eff.cloneCustom());
+                predicatesSatisfy.addAll(valid);
+                boolean reInsert = true;
+                for (Predicate sat: predicatesSatisfy) {
+                    if (sat.getParams().containsAll(p.getParams())) {
+                        reInsert = false;
+                    }
                 }
-                ArrayList newParams = new ArrayList();
-                
-                for (Variable v: availableVariables) {
-                    newParams.add(v.getVar());
+                if (reInsert == true) {
+                    stack.add(p);
                 }
-                for (Variable v: availableVariables) {
-                    newParams.set(v.getIndex(), v.getVar());
+                
+                if (predicatesSatisfy.size() == a.getPrecond().size() - 1) {
+                    
+                    for (Predicate validated : predicatesSatisfy) {
+                        if (validated.getIdentifier().equals("On")) {
+                            newParams = validated.getParams();
+                            p.setParams(newParams);
+                        }
+                    }
                 }
-                newAction.setParams(action.getParams());
-                newAction.setPrecond(preconds);
-                newAction.setEffects(effects);
-                //magia
-                boolean valid1 = modifyAction(newAction, newParams, newAction.getPrecond());
-                boolean valid2 = modifyAction(newAction, newParams, newAction.getEffects());
-                
-                newAction.setParams(newParams);
-               
-                
-                
-               
-                returnArray.add(newAction);
+            }
+            
+            
+          
+        }
+        if (!newParams.equals(action_params)) {
+            for (Predicate precond: predicates) {
+                for (int i = 0; i < action_params.size(); i++) {
+                    for (int j = 0; j < precond.getParams().size();j++) {
+                        if ( precond.getParams().get(j).equals(action_params.get(i)) && precond.valid(j)) {
+                            precond.getParams().set(j, newParams.get(i));
+                            precond.addEvalIndex(j);
+                        }
+                    }
+                }
+                precond.clearEval();
             }
         }
-        return returnArray;
+        return true;
     }
     
     
